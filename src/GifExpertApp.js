32698f964bbb0b27add85e1afc7683cc58b856c9
Maximino Castillo <maxcastgarcia@gmail.com>
2021-06-10 16:25:14 -0500

    Upload the project

---
+++ src/GifExpertApp.js
@@ -0,0 +1,26 @@
+import React, { useState } from 'react';
+import AddCategory from './components/AddCategory';
+import GifGrid from './components/GifGrid';
+
+export const GifExpertApp = () => {
+  const [categories, setCategories] = useState(['Dragon Ball']);
+
+  return(
+    <>
+      <h2>GifExpertApp</h2>
+      <hr /> 
+      <AddCategory setCategories={setCategories}/>
+      <ol>
+        {
+          categories.map(category => (
+            <GifGrid 
+              key={category}
+              category={category}/>
+          ))
+        }
+      </ol>
+    </>
+  );
+}
+
+export default GifExpertApp;
